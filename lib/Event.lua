abc5f919b9759387c4b370ce56e0cda119e251d0
Possseidon <xpossseidon@gmail.com>
2021-01-12 20:24:04 +0100

    Added emmy annotations to Event.lua.

--- lib/Event.lua
+++ lib/Event.lua
@@ -1,11 +1,18 @@
 local class = require "class"
 
+---@class Event: class
+---@type fun(): Event
+---@type fun(...)
 local Event = class("Event")
 
+---Appends a new event handler.
+---@param handler any
 function Event:add(handler)
   table.insert(self, handler)
 end
 
+---Removes an existing event handler, raising an error if it doesn't actually exist.
+---@param handler any
 function Event:remove(handler)
   for i = #self, 1, -1 do
     if self[i] == handler then
@@ -16,6 +23,8 @@ function Event:remove(handler)
   error("event handler not found", 2)
 end
 
+---Calls all event handlers in the order they were added with the given parameters.
+---@vararg any
 function Event.metatable:__call(...)
   for i = 1, #self do
     self[i](...)
